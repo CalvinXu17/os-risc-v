0ca102d60b8fd82159871d2117b9c582f95fc04e
calvinxu17 <1084240577@qq.com>
2021-04-22 18:51:26 +0800

    补充kmalloc的锁

--- os/kernel/kmalloc.c
+++ os/kernel/kmalloc.c
@@ -1,6 +1,9 @@
 #include "kmalloc.h"
 #include "slob.h"
 #include "page.h"
+#include "spinlock.h"
+
+spinlock kmalloc_lock = {"kmalloc_lock", 0, 0};
 
 static uint64 align16(uint64 size)
 {
@@ -21,7 +24,8 @@ void *kmalloc(uint64 size)
 	small_block *m;
 	big_block *bigblock;
     size = align16(size);
-	if (size < PAGE_SIZE - UNIT_SIZE) {
+	if (size < PAGE_SIZE - UNIT_SIZE)
+	{
 		m = small_alloc(size + UNIT_SIZE);
         if(!m) return 0;
         else return (void*)(m+1);
@@ -34,11 +38,14 @@ void *kmalloc(uint64 size)
 	bigblock->page_n = page_n;
 	bigblock->pages = (void *)slob_get_pages(page_n);
 
-	if (bigblock->pages) { // 分配n页成功
-		// spin_lock_irqsave(&block_lock, flags);
+	if (bigblock->pages) // 分配n页成功
+	{
+		lock(&kmalloc_lock);
+
 		bigblock->next = p_big_blocks;
 		p_big_blocks = bigblock;
-		// spin_unlock_irqrestore(&block_lock, flags);
+
+		unlock(&kmalloc_lock);
 		return bigblock->pages;
 	}
 
@@ -54,18 +61,21 @@ void kfree(void *addr)
 	if (!addr)
 		return;
 
-	if (!((uint64)addr & (PAGE_SIZE-1))) { // 如果block的地址按页对齐，则说明这个block可能是big_block，但也可能不是，因此需要查找p_big_blocks
-		// spin_lock_irqsave(&block_lock, flags);
-		for (bigblock = p_big_blocks; bigblock != 0; last = &bigblock->next, bigblock = bigblock->next) {
-			if (bigblock->pages == addr) {
+	if (!((uint64)addr & (PAGE_SIZE-1))) // 如果block的地址按页对齐，则说明这个block可能是big_block，但也可能不是，因此需要查找p_big_blocks
+	{
+		lock(&kmalloc_lock);
+		for (bigblock = p_big_blocks; bigblock != 0; last = &bigblock->next, bigblock = bigblock->next)
+		{
+			if (bigblock->pages == addr)
+			{
 				*last = bigblock->next;
-				// spin_unlock_irqrestore(&block_lock, flags);
+				unlock(&kmalloc_lock);
 				slob_free_pages((uint64)addr, bigblock->page_n);
 				small_free(bigblock, sizeof(big_block));
 				return;
 			}
 		}
-		// spin_unlock_irqrestore(&block_lock, flags);
+		unlock(&kmalloc_lock);
 	}
 
 	small_free(((small_block *)addr) - 1, 0);
@@ -78,14 +88,18 @@ uint64 ksize(const void *addr)
 	if (!addr)
 		return 0;
 
-	if (!((uint64)addr & (PAGE_SIZE-1))) {
-		//spin_lock_irqsave(&block_lock, flags);
+	if (!((uint64)addr & (PAGE_SIZE-1)))
+	{
+		lock(&kmalloc_lock);
+		
 		for (bigblock = p_big_blocks; bigblock != 0; bigblock = bigblock->next)
-			if (bigblock->pages == addr) {
-				// spin_unlock_irqrestore(&slob_lock, flags);
+			if (bigblock->pages == addr)
+			{
+				unlock(&kmalloc);
 				return bigblock->page_n * PAGE_SIZE;
 			}
-		//spin_unlock_irqrestore(&block_lock, flags);
+		
+		unlock(&kmalloc_lock);
 	}
 
 	return (((small_block *)addr) - 1)->unit_n * UNIT_SIZE;
